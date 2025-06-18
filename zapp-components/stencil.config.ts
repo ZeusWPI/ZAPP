5077254fdf264f9ad0a5d1d00263cc3cb7fb9768
robinpdev <abcd@efg.com>
2025-06-19 00:05:26 +0200

    init met example dropdown but no worky

---
+++ zapp-components/stencil.config.ts
@@ -0,0 +1,31 @@
+import { Config } from '@stencil/core';
+import tailwind, { tailwindHMR } from 'stencil-tailwind-plugin';
+
+export const config: Config = {
+  namespace: 'zapp-components',
+  plugins: [
+    tailwind(),
+    tailwindHMR(),
+  ],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "shell",
+  },
+};
